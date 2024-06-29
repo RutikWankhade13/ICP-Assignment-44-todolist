9de1aba6a2f1f74aa50d32008a6cd88ecbde2cb7
RutikWankhade13 <rutikwankhade81@gmail.com>
2024-06-29 21:09:58 +0530

    added categories

--- src/components/todoCard/todoCard.js
+++ src/components/todoCard/todoCard.js
@@ -10,12 +10,21 @@ function todoCard({task,categeory}) {
         Shopping: "🛒",
         Other: "😁"
     }
-
+    const Category_Color = {
+        Sports: "#008080",
+        Learning: "#4d4dff",
+        Work: "#86b300",
+        Personal: "#0099ff",
+        Shopping: "#cc00cc",
+        Health: "#00e600",
+        Other: "#ffad33"
+    }
   return (
     <div className="todo-Card">
         {task}
         <span className="category">
             {categeory}
+            <span className="category" style={{backgroundColor: Category_Color[categeory]}}></span>
         {Category_Emoji[categeory]} {categeory}
         </span>
     </div>
