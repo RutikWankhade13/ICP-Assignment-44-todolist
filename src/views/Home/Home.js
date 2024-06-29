6bc12200d5f14efddab9f1ce660a2f6749af1274
RutikWankhade13 <rutikwankhade81@gmail.com>
2024-06-29 20:34:05 +0530

    created basis todolist

---
+++ src/views/Home/Home.js
@@ -0,0 +1,34 @@
+import "./Home.css"
+import add from "./add.png"
+import ToDoCard from "../../components/todoCard/todoCard"
+function Home() {
+    const todolist = [
+        "Go to gym",
+        "buy grocery",
+        "finish project",
+        "household",
+        "pay bill",
+        "watch movie"
+    ]
+
+  return (
+    <div>
+        <h1 className="app-title">To-Do App📝</h1>
+        <div className="todolist-container">
+            <h2>Add Your new Task</h2>
+           {
+            todolist.map((todoItem, i)=>{
+                return(
+                    <ToDoCard key={i} todoItem={todoItem}/>
+                )
+            })
+           }
+        </div>
+        <div className="add-item-container">
+            <input type="text" placeholder="Add your Task" className="add-input"/>
+            <img src={add} alt="add-btn" className="add-icon"/>
+        </div>
+    </div>
+  )
+}
+export default Home
