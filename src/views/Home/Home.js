94f5175ff58d840cc1a5083cd300fa08466cf492
RutikWankhade13 <rutikwankhade81@gmail.com>
2024-06-29 20:44:09 +0530

    added new task adding feature

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,23 +1,27 @@
 import "./Home.css"
 import add from "./add.png"
 import ToDoCard from "../../components/todoCard/todoCard"
+import { useState } from "react"
 function Home() {
     const todolist = [
         "Go to gym",
         "buy grocery",
+    ]
+    const [todoList, setTodoList] = useState([
         "finish project",
         "household",
         "pay bill",
         "watch movie"
-    ]
+    ])
+    const [newTask, setNewTask] = useState("")
 
   return (
     <div>
         <h1 className="app-title">To-Do App📝</h1>
         <div className="todolist-container">
-            <h2>Add Your new Task</h2>
            {
-            todolist.map((todoItem, i)=>{
+         
+            todoList.map((todoItem, i)=>{
                 return(
                     <ToDoCard key={i} todoItem={todoItem}/>
                 )
@@ -25,10 +29,17 @@ function Home() {
            }
         </div>
         <div className="add-item-container">
-            <input type="text" placeholder="Add your Task" className="add-input"/>
+            
             <img src={add} alt="add-btn" className="add-icon"/>
+            <input
+                type="text"
+                placeholder="Add your Task"
+                className="add-input"
+                value={newTask}
+                onChange={(e)=>setNewTask(e.target.value)}
+            />
+          
         </div>
     </div>
-  )
-}
+)}
 export default Home
